1deb5acd3307383023f5adf1a1989c96f159a2ab
Chris Buza <codeweasel@gmail.com>
2026-05-30 21:24:12 -0400

    For issue #16: Moved current genetic code into its own Core module

--- src/Core/Genetics/IntegerGene.cs
+++ src/Core/Genetics/IntegerGene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AmoebaSim.Desktop
+namespace AmoebaSim.Core.Genetics
 {
     public class IntegerGene : IGene
     {
